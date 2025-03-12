744afd95181c971320f3d021dfc30e0332257886
BeingSaurabh2424 <saurabhwadekar24@gmail.com>
2025-03-12 15:22:34 +0530

    day3_2

--- Day_3/day3_2.cpp
+++ Day_3/day3_2.cpp
@@ -1,42 +1,59 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-
 
 class Time
 {
     int hr;
     int min;
-    int sec;
 
 public:
-    void accept()
+    // constructor -> initialization of the state of an object
+    // parameterless ctor
+    Time()
     {
-        cout<<"Enter the hrs"<<endl;
-        cin>>hr;
-
-        cout<<"Enter thr mins "<<endl;
-        cin>>min;
-
-        cout<<"Enter thr secs "<<endl;
-        cin>>sec;
-
+        cout << "Inside Paramterless Ctor" << endl;
+        hr = 10;
+        min = 10;
     }
 
+    Time(int value)
+    {
+        cout << "Inside Single Parmeterized Ctor" << endl;
+        hr = value;
+        min = value;
+    }
+
+    Time(int hr, int min)
+    {
+        cout << "Inside parameterized Ctor" << endl;
+        this->hr = hr;
+        this->min = min;
+    }
+
+    void initTime()
+    {
+        hr = 10;
+        min = 10;
+    }
     void display()
     {
-        cout<<"Clock :"<<hr<<":"<<min<<":"<<sec<<endl;
+        cout << "Time - " << hr << " : " << this->min << endl;
     }
-
 };
-
 
 int main()
 {
-    Time t1;
+    Time t1; // without passing any arguments
     t1.display();
-    
     Time t2;
     t2.display();
+    Time t3(11, 30); // by passing the arguments
+    Time t4(12, 40);
+
+    t3.display();
+    t4.display();
+
+    Time t5(11);
+    t5.display();
     return 0;
 }
