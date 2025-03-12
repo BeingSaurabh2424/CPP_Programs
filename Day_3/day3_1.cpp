ba41dd144c62b125b74839cab9634130d0da40f0
BeingSaurabh2424 <saurabhwadekar24@gmail.com>
2025-03-12 14:55:36 +0530

    day3_1.cpp

--- Day_3/day3_1.cpp
+++ Day_3/day3_1.cpp
@@ -12,19 +12,19 @@ public:
     void accept()
     {
         cout<<"Enter the hrs "<<endl;
-        cin>>hr;
+        cin>>this->hr;
 
-        cout<<"Enter the hrs "<<endl;
-        cin>>hr;
+        cout<<"Enter the mins "<<endl;
+        cin>>this->hr;
 
-        cout<<"Enter the hrs "<<endl;
-        cin>>hr;
+        cout<<"Enter the secs "<<endl;
+        cin>>this->hr;
 
     }
 
     void display()
     {
-        cout<<"Clock "<<hr<<":"<<min<<":"<<sec<<endl;
+        cout<<"Clock "<<hr<<":"<<this->min<<":"<<sec<<endl;
     }
 
 };
@@ -38,4 +38,16 @@ int main()
 
     
     return 0;
+}
+
+int main1()
+{
+    Time t1;
+    Time t2;
+
+    cout<<"Address of t1 in main "<<&t1<<endl;
+    cout<<"Address of t2 in main "<<&t2<<endl;
+
+    t1.accept();
+    t1.display();
 }
